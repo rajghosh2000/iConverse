bbd0bcab43cd81c35a8d1d7a6f27ceea85198baf
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2020-06-22 20:37:30 +0530

    updates

--- index.php
+++ index.php
@@ -16,6 +16,39 @@
     <?php include 'partials/_header.php';?>
 
     
+    
+        <section class="text-gray-500 bg-gray-900 body-font">
+        <div class="container-fluid px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-4">
+        <!-- Use a for loop to iterate through the categories -->
+
+        
+            
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://source.unsplash.com/420x260/?coding,java">
+                    </a>
+                <div class="mt-4">
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                    <h2 class="text-white title-font text-lg font-medium">The Catalyzer</h2>
+                    
+                    <button type="button" class="btn btn-outline-success btn-sm my-2">Let's Explore</button>
+                </div>
+                </div>
+                
+
+
+
+
+
+            </div>
+        </div>
+        </section>
+    
+    <hr>
+    
+    
+    </div>
 
     <?php include 'partials/_footer.php';?>
 
---
+++ partials/_dbconnect.php
@@ -0,0 +1,6 @@
+<?php
+
+//Connecting to database
+
+
+?>
