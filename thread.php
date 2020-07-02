9981fe6e4ff73c34d141351f9542aaac70e84c63
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2020-07-02 22:23:23 +0530

    ....

--- thread.php
+++ thread.php
@@ -46,6 +46,28 @@ hr {
     
     ?>
 
+
+    <?php
+        $showAlert = false;
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method =='POST')
+        {
+            $ans = $_POST['cmt'];
+            $sql2 = "INSERT INTO `answers` (`ans_info`, `ans_thread_id`, `ans_by`, `ans_time`) VALUES ('$ans', '$id', '0', current_timestamp());";
+            $res2 = mysqli_query($con,$sql2);
+            $showAlert = true;
+            if($showAlert)
+            {
+                echo '<div class="flex items-center bg-green-500 text-blue-900 text-text-2xl font-bold px-4 py-16" role="alert">
+                <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <p ><strong>SUCCESS!!!!!  </strong>Reply added.</p>
+              </div>'; 
+            }
+           
+        }
+    ?>
+
+
     <section class="text-gray-500 bg-gray-900 body-font">
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap -m-4">
@@ -105,6 +127,7 @@ hr {
                    $noAns = false;
                    $ans_info = $row['ans_info'];
                    $ans_id = $row['ans_id'];
+                   $ans_time = $row['ans_time'];
                    $cnt = $cnt + 1;
                    echo '<div class="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
                    <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
@@ -123,7 +146,7 @@ hr {
                            </svg>
                        </div>
                        <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
-                           <h2 class="font-medium title-font text-white mb-1 text-xl">RAJ</h2>
+                           <h2 class="font-medium title-font text-white mb-1 text-xl">RAJ  <span class ="text-base text-green-500">at '.$ans_time.'</span></h2>
                            <p class="leading-relaxed">'.$ans_info.'</p>
                        </div>
                    </div>
